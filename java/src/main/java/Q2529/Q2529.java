c138ed72fa9eb8d1459684420883a59d73270961
Zhenghao Lou <rhzx3519@gmail.com>
2024-04-09 21:08:54 +1000

    Q2529

---
+++ java/src/main/java/Q2529/Q2529.java
@@ -0,0 +1,19 @@
+package Q2529;
+
+public class Q2529 {
+}
+
+class Solution {
+    public int maximumCount(int[] nums) {
+        int pos = 0;
+        int neg = 0;
+        for (int x : nums) {
+            if (x > 0) {
+                pos++;
+            } else if (x < 0) {
+                neg++;
+            }
+        }
+        return Math.max(pos, neg);
+    }
+}
