1659288456d6c46a270263da87540f61841a2eed
Zhenghao Lou <rhzx3519@gmail.com>
2024-05-21 10:59:38 +1000

    2769

---
+++ go/src/leetcode/2769/2769.go
@@ -0,0 +1,8 @@
+package main
+
+/**
+https://leetcode.cn/problems/find-the-maximum-achievable-number/?envType=daily-question&envId=2024-05-21
+*/
+func theMaximumAchievableX(num int, t int) int {
+    return num + t<<1
+}
