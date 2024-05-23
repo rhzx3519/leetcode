63b482b6207dc648edba703f3ee8100d000189e8
Zhenghao Lou <rhzx3519@gmail.com>
2024-05-24 09:25:07 +1000

    1673

---
+++ go/src/leetcode/1673/1673.go
@@ -0,0 +1,17 @@
+package main
+
+/**
+https://leetcode.cn/problems/find-the-most-competitive-subsequence/?envType=daily-question&envId=2024-05-24
+*/
+func mostCompetitive(nums []int, k int) []int {
+    nk := len(nums) - k
+    var st []int
+    for i := range nums {
+        for nk != 0 && len(st) != 0 && st[len(st)-1] > nums[i] {
+            nk--
+            st = st[:len(st)-1]
+        }
+        st = append(st, nums[i])
+    }
+    return st[:k]
+}
