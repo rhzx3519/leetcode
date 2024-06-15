f36d0a71324b4d8d84e822cdc95fc4a182347f60
Zhenghao Lou <rhzx3519@gmail.com>
2024-06-15 10:05:11 +1000

    2779

---
+++ go/src/leetcode/2779/2779.go
@@ -0,0 +1,18 @@
+package main
+
+import "sort"
+
+/**
+https://leetcode.cn/problems/maximum-beauty-of-an-array-after-applying-operation/?envType=daily-question&envId=2024-06-15
+*/
+func maximumBeauty(nums []int, k int) (tot int) {
+    sort.Ints(nums)
+    var l int
+    for r := range nums {
+        for nums[r]-nums[l] > k*2 {
+            l++
+        }
+        tot = max(tot, r-l+1)
+    }
+    return
+}
