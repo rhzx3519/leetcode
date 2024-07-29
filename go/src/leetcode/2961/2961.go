9e5a6c9fa74091e817bd12c63bdba5840797e5db
Zhenghao Lou <rhzx3519@gmail.com>
2024-07-30 06:37:34 +1000

    2961

---
+++ go/src/leetcode/2961/2961.go
@@ -0,0 +1,24 @@
+package main
+
+/**
+https://leetcode.cn/problems/double-modular-exponentiation/?envType=daily-question&envId=2024-07-30
+*/
+func getGoodIndices(variables [][]int, target int) (ans []int) {
+    for i, v := range variables {
+        if pow(pow(v[0], v[1], 10), v[2], v[3]) == target {
+            ans = append(ans, i)
+        }
+    }
+    return
+}
+
+func pow(x, n, mod int) int {
+    res := 1
+    for ; n > 0; n /= 2 {
+        if n%2 > 0 {
+            res = res * x % mod
+        }
+        x = x * x % mod
+    }
+    return res
+}
